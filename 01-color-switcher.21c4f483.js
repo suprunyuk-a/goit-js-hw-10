9c04d9012c83097537518130ce21bd08a84ba3f5
suprunyuk-a <103211775+suprunyuk-a@users.noreply.github.com>
2022-09-07 13:05:59 +0000

    Deploying to gh-pages from @ suprunyuk-a/goit-js-hw-10@5402da377fed8ccefdee0b2c772727af1f6e0df4 🚀

--- 01-color-switcher.21c4f483.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let r;t.addEventListener("click",(e=>{r=setInterval((()=>{t.setAttribute("disabled",""),document.querySelector("body").setAttribute("style",`background-color:#${Math.floor(16777215*Math.random()).toString(16)}`)}),1e3)})),e.addEventListener("click",(e=>{clearInterval(r),t.removeAttribute("disabled")}));
-//# sourceMappingURL=01-color-switcher.21c4f483.js.map
