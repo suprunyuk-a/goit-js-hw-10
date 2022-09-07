6054536b7a3f6841e74e8da5844ca367059f1c22
suprunyuk-a <chaserenergy@gmail.com>
2022-09-07 16:10:01 +0300

    edit_name

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-import "./css/styles.css";
-import Notiflix from "notiflix";
-import debounce from "lodash.debounce";
-import { fetchCountries } from "./js/fetchCountries";
-import { renderCityList } from "./js/renderCityList";
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
+import { renderCityList } from './js/renderCityList';
 
-const fetchCity = document.querySelector("#search-box");
-const cityList = document.querySelector(".country-list");
-const cityInfo = document.querySelector(".country-info");
+const fetchCity = document.querySelector('#search-box');
+const cityList = document.querySelector('.country-list');
+const cityInfo = document.querySelector('.country-info');
+let nameCity = '';
 
 const debouceFunct = debounce(() => {
   (nameCity = handleKeyPress()),
     fetchCountries(nameCity)
-      .then((cities) => renderCityList(cities))
+      .then(cities => renderCityList(cities))
       .catch(
-        (error) =>
-          Notiflix.Notify.failure(" Oops, there is no country with that name"),
-        ((cityList.innerHTML = ""), (cityInfo.innerHTML = ""))
+        error =>
+          Notiflix.Notify.failure(' Oops, there is no country with that name'),
+        ((cityList.innerHTML = ''), (cityInfo.innerHTML = ''))
       );
 }, 300);
 
-fetchCity.addEventListener("input", debouceFunct);
+fetchCity.addEventListener('input', debouceFunct);
 
-const handleKeyPress = (event) => {
+const handleKeyPress = event => {
   return fetchCity.value.trim();
 };
