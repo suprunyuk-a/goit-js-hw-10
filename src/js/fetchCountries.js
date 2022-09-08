2ea215828a05832fdf2a54029ee1c0465c488a79
suprunyuk-a <chaserenergy@gmail.com>
2022-09-08 12:46:04 +0300

    edit_404

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,11 +1,12 @@
-function fetchCountries(nameCity) {
-  const url = `https://restcountries.com/v3.1/name/${nameCity}?name,capital,flags,population,languages`;
-  return fetch(url).then((response) => {
+async function fetchCountries(nameCity) {
+  if (nameCity.length > 0) {
+    const url = `https://restcountries.com/v3.1/name/${nameCity}?name,capital,flags,population,languages`;
+    const response = await fetch(url);
     if (!response.ok) {
       throw new Error(response.status);
     }
-    return response.json();
-  });
+    return await response.json();
+  }
 }
 
 export { fetchCountries };
